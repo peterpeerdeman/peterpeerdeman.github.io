c31951507c84183dc07283e967956507da499e00
Peter Peerdeman <peterpeerdeman@gmail.com>
2022-02-27 23:14:35 +0100

    content(projects): add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -7,9 +7,27 @@ const projectsData = [
     },
     {
         title: 'Snoffeecob',
-        description: `A coffeeblog dedicated to my journey into the world of specialty coffee, and the different methods of brewing espresso and pourover`,
+        description: `A coffeeblog dedicated to my journey into the world of specialty coffee, and the different methods of brewing espresso and pourover, written with NodeJS & Gatsby`,
         imgSrc: '/static/img/snoffeecob.png',
         href: 'https://snoffeecob.com',
+    },
+    {
+        title: 'Timeseries & Quantified Self',
+        description: `The collection of timeseries data of about any (digital) aspect of my life, including but not limited to health, parking, notes, gaming, devices data, gaming, finance using NodeJS, influxdb, grafana`,
+        imgSrc: '/assets/images/2020-05-20-destiny-1.png',
+        href: '/blog/rasplogger-influxdb-grafana-dashboards-showcase',
+    },
+    {
+        title: 'Electronic Music Visualisation',
+        description: `Real-time generated visualisations based on midi data to accompany electronic live music sets, written with Processing.`,
+        imgSrc: '/static/img/electroniclivesetvisualisation.jpg',
+        href: 'https://www.youtube.com/watch?v=qtN9qzKjo_U',
+    },
+    {
+        title: 'Home Automation & Wearable app',
+        description: `The creation of an API & wearable app that aggregates all sorts of home automation and statics about lights, thermostat, music, pv, cluster and others. Written with TypeScript & NodeJS`,
+        imgSrc: '/static/img/raspapi-fitbit.jpg',
+        href: 'https://github.com/peterpeerdeman/fitbit-raspi',
     },
     {
         title: 'Recordfairs',
@@ -19,7 +37,7 @@ const projectsData = [
     },
     {
         title: 'Wisdoms',
-        description: `A platform to record the wisdoms and quotes you've stumbled upon and share them with the world, originally written in Ruby on Rails`,
+        description: `A platform to record the wisdoms and quotes you've stumbled upon and share them with the world, originally written in Ruby on Rails, now discontinued`,
         imgSrc: '/static/img/wisdoms.png',
         href: 'https://wisdoms.herokuapp.com',
     },
