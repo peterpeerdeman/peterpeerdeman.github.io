5243410c0bb112505a929a712b032a0d98212161
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-02-15 10:51:31 +0100

    fix(videos): fix vimeo and youtube videos in blogs

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,14 +1,27 @@
 type Props = {
   id: string
+  mediaprovider: 'youtube' | 'vimeo'
 }
 
-export default function Video({ id }: Props) {
-  return (
-    <div className="aspect-w-16 aspect-h-9">
-      <iframe
-        src={`https://player.vimeo.com/video/${id}?h=7f7f1bd9e7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
-        title={`video ${id} on vimeo`}
-      ></iframe>
-    </div>
-  )
+export default function Video({ id, mediaprovider }: Props) {
+  switch (mediaprovider) {
+    case 'youtube':
+      return (
+        <iframe
+          width="640"
+          height="480"
+          src={`https://www.youtube.com/embed/${id}`}
+          title={`video ${id} on youtube`}
+        ></iframe>
+      )
+    case 'vimeo':
+      return (
+        <iframe
+          width="640"
+          height="480"
+          src={`https://player.vimeo.com/video/${id}?h=7f7f1bd9e7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
+          title={`video ${id} on vimeo`}
+        ></iframe>
+      )
+  }
 }
--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src player.vimeo.com
+  frame-src player.vimeo.com www.youtube.com
 `
 
 const securityHeaders = [
